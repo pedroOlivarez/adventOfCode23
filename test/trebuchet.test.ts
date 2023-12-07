1f970f2d8b9c1d0de209a2c1559dfe489e2063f0
pedroOlivarez <pedro.olivarezjr@gmail.com>
2023-12-07 00:25:31 -0600

    develop: explicit test value

--- test/trebuchet.test.ts
+++ test/trebuchet.test.ts
@@ -4,7 +4,6 @@ import { input } from "../input/trebuchet/input";
 describe('Day 1', () => {
     it('Will compute the right answer', () => {
         const result = trebuchet(input);
-        console.log(result);
-        expect(result).toBeTruthy();
+        expect(result).toBe(53921);
     })
 });
